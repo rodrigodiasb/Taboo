160a3338dc67096ae8914d35d09073bad0b4d9ec
rodrigodiasb <62857786+rodrigodiasb@users.noreply.github.com>
2025-11-17 22:26:59 -0400

    Update sound.js

--- assets/js/modules/sound.js
+++ assets/js/modules/sound.js
@@ -1,19 +1,31 @@
 export const Sound = {
-  beep() {
+  beep() { /* igual antes */ },
+  end() { /* igual antes */ },
+
+  pular() {
     const ctx = new AudioContext();
     const osc = ctx.createOscillator();
-    osc.frequency.value = 900;
+    osc.frequency.value = 600;
     osc.connect(ctx.destination);
     osc.start();
-    osc.stop(ctx.currentTime + 0.1);
+    osc.stop(ctx.currentTime + 0.15);
   },
 
-  end() {
+  taboo() {
     const ctx = new AudioContext();
     const osc = ctx.createOscillator();
-    osc.frequency.value = 300;
+    osc.frequency.value = 200;
     osc.connect(ctx.destination);
     osc.start();
-    osc.stop(ctx.currentTime + 0.5);
+    osc.stop(ctx.currentTime + 0.2);
+  },
+
+  acerto() {
+    const ctx = new AudioContext();
+    const osc = ctx.createOscillator();
+    osc.frequency.value = 1000;
+    osc.connect(ctx.destination);
+    osc.start();
+    osc.stop(ctx.currentTime + 0.2);
   }
 };
