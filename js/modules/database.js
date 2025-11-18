7ae246fbcb308378eb127b0343e04bc4e9aad4b3
rodrigodiasb <62857786+rodrigodiasb@users.noreply.github.com>
2025-11-17 22:17:17 -0400

    Delete js directory

--- js/modules/database.js
+++
@@ -1,9 +0,0 @@
-import cards from '../data/cards.json' assert { type: 'json' };
-
-export const Database = {
-  cards: cards.cartas,
-  getAll(mode='all') {
-    if(mode === 'all') return this.cards;
-    return this.cards;
-  }
-};
